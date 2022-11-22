2c41cef764dba8e24e64ce37f8418712e57ca886
matiasjokela <matias.jokela@outlook.com>
2022-11-22 18:38:31 +0200

    Fixed re-rendering issue when stats are zeroed

--- src/components/StatsPage.js
+++ src/components/StatsPage.js
@@ -15,13 +15,40 @@ import userService from "../services/user";
 const StatsPage = () => {
   const [view, setView] = useState("Stats");
   const [user, setUser] = useState(null);
+  const [physics, setPhysics] = useState("Ei pelejä");
+  const [chemistry, setChemistry] = useState("Ei pelejä");
+  const [biology, setBiology] = useState("Ei pelejä");
+  const [best, setBest] = useState("Ei pelejä");
+
   const localUser = JSON.parse(
     window.localStorage.getItem("loggedExamPrepUser")
   ); // Tee tämä staten avulla mieluummin??
 
   useEffect(() => {
-    userService.getById(localUser.id).then((response) => setUser(response));
+    userService.getById(localUser.id).then((response) => {
+      setUser(response);
+    });
   }, []);
+
+  if (user && best == "Ei pelejä") {
+    if (user.physicsTotal) {
+      setPhysics(`${user.physicsCorrect} / ${user.physicsTotal} (
+			${(user.physicsCorrect / user.physicsTotal) * 100} %)`);
+    }
+    if (user.chemistryTotal) {
+      setChemistry(`${user.chemistryCorrect} / ${user.chemistryTotal} (
+			  ${(user.chemistryCorrect / user.chemistryTotal) * 100} %)`);
+    }
+    if (user.biologyTotal) {
+      setBiology(`${user.biologyCorrect} / ${user.biologyTotal} (
+			  ${(user.biologyCorrect / user.biologyTotal) * 100} %)`);
+    }
+    if (user.bestTotal) {
+      setBest(`${user.bestCorrect} / ${user.bestTotal} (
+			${(user.bestCorrect / user.bestTotal) * 100} %, 
+			${user.bestCategory})`);
+    }
+  }
 
   // Miten saatiinkaan sivu refreshaamaan fiksusti
   const handleZero = () => {
@@ -40,6 +67,11 @@ const StatsPage = () => {
         },
         user.id
       );
+      setPhysics("Ei pelejä");
+      setChemistry("Ei pelejä");
+      setBiology("Ei pelejä");
+      setBest("Ei pelejä");
+      setUser(null);
     }
   };
 
@@ -48,58 +80,67 @@ const StatsPage = () => {
   }
 
   // Tilastoille joku siistimpi esitytapa, taulukko tms.
-  if (user) {
-    return (
-      <Container className="p-sm-3 p-0 align-items-center card_view">
-        <Card className="mb-3 mx-auto w-100 shadow card_view align-items-center">
-          <Card.Body>
-            <ButtonGroup>
-              <Button
-                className="d-grid gap-2 mx-auto w-100"
-                variant="outline-dark"
-                onClick={() => setView("Landing")}
-              >
-                Takaisin
-              </Button>
-              <Button
-                className="d-grid gap-2 mx-auto w-100"
-                variant="outline-dark"
-                onClick={() => handleZero()}
-              >
-                Nollaa tilastot
-              </Button>
-            </ButtonGroup>
 
-            <Card.Title
-              className="fs-5 d-flex mb-4 mx-auto card_view align-items-center"
-              style={{ height: "3rem" }}
+  return (
+    <Container className="p-sm-3 p-0 align-items-center card_view">
+      <Card className="mb-3 mx-auto w-100 shadow card_view align-items-center">
+        <Card.Body>
+          <ButtonGroup>
+            <Button
+              className="d-grid gap-2 mx-auto w-100"
+              variant="outline-dark"
+              onClick={() => setView("Landing")}
             >
-              Tilastot
-            </Card.Title>
-            <Card.Text>
-              <Row>
-                Fysiikka: {user.physicsCorrect} / {user.physicsTotal} (
-                {(user.physicsCorrect / user.physicsTotal) * 100} %)
-              </Row>
-              <Row>
-                Kemia: {user.chemistryCorrect} / {user.chemistryTotal} (
-                {(user.chemistryCorrect / user.chemistryTotal) * 100} %)
-              </Row>
-              <Row>
-                Biologia: {user.biologyCorrect} / {user.biologyTotal} (
-                {(user.biologyCorrect / user.biologyTotal) * 100} %)
-              </Row>
-              <Row>
-                Paras: {user.bestCorrect} / {user.bestTotal} (
-                {(user.bestCorrect / user.bestTotal) * 100} %,{" "}
-                {user.bestCategory})
-              </Row>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
-    );
-  }
+              Takaisin
+            </Button>
+            <Button
+              className="d-grid gap-2 mx-auto w-100"
+              variant="outline-dark"
+              onClick={() => handleZero()}
+            >
+              Nollaa tilastot
+            </Button>
+          </ButtonGroup>
+
+          <Card.Title
+            className="fs-5 d-flex mb-4 mx-auto card_view align-items-center"
+            style={{ height: "3rem" }}
+          >
+            Tilastot
+          </Card.Title>
+          <Card.Text>
+            <Row>Fysiikka: {physics}</Row>
+            <Row>Kemia: {chemistry}</Row>
+            <Row>Biologia: {biology}</Row>
+            <Row>Paras: {best}</Row>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
 
 export default StatsPage;
+
+/*
+</Card.Title>
+<Card.Text>
+  <Row>
+	Fysiikka: {user.physicsCorrect} / {user.physicsTotal} (
+	{(user.physicsCorrect / user.physicsTotal) * 100} %)
+  </Row>
+  <Row>
+	Kemia: {user.chemistryCorrect} / {user.chemistryTotal} (
+	{(user.chemistryCorrect / user.chemistryTotal) * 100} %)
+  </Row>
+  <Row>
+	Biologia: {user.biologyCorrect} / {user.biologyTotal} (
+	{(user.biologyCorrect / user.biologyTotal) * 100} %)
+  </Row>
+  <Row>
+	Paras: {user.bestCorrect} / {user.bestTotal} (
+	{(user.bestCorrect / user.bestTotal) * 100} %,{" "}
+	{user.bestCategory})
+  </Row>
+</Card.Text>
+*/
